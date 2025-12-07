60641092f20613e756918e4cc40eb3db88af0e5d
ViktoriaElm <v.gorkina.job@gmail.com>
2025-12-07 20:40:39 +0300

    feat(popups): updated close button markup, implemented category filtering, duplication and shuffling of cards in src/components/popups/GameFieldPopup.tsx

--- src/components/popups/GameFieldPopup.tsx
+++ src/components/popups/GameFieldPopup.tsx
@@ -1,42 +1,60 @@
 import popupStyles from "./popup.module.scss";
 import Card from "../cards/Card";
+import data from "../../../data/data.json";
+import { useEffect, useState } from "react";
 
 interface GameFieldPopupProps {
     closePopup: () => void;
     children: React.ReactNode;
 }
 
+type DoubledDataItem = typeof data[number];
+
+function shuffleArray<T>(array: T[]): T[] {
+  let currentIndex = array.length;
+  while (currentIndex !== 0) {
+    const randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+  return array;
+}
+
 export default function GameFieldPopup({ closePopup, children }: GameFieldPopupProps) {
+
+    const [shuffledData, setShuffledData] = useState<DoubledDataItem[]>([])
+
+    const filteredAnimeData = data.filter(item => item.category === "anime");
+
+    const doubleAnimeData = filteredAnimeData.concat(filteredAnimeData.slice());
+
+
+    useEffect(() => {
+        const shuffled = shuffleArray(doubleAnimeData);
+        setShuffledData(shuffled)
+    }, [])
+
 
     return (
         <div className={`${popupStyles.popup__container} ${popupStyles.popup__container_cardStyles}`}>
 
-            <button
+            <span
                 className={popupStyles.popup__closeBtn}
                 onClick={closePopup}
             >
-                X
-            </button>
+                x
+            </span>
 
             <h2 className={popupStyles.popup__h2}>
                 Уровень: {children}
             </h2>
 
             <div className={`${popupStyles.popup__cardContainer}`}>
-                <Card src="public/zebra.jpg"/>
-                <Card src="public/fox.jpg"/>
-                <Card src="public/kenguru.jpg"/>
-                <Card src="public/fox.jpg"/>
-                <Card src="public/gepard.jpg"/>
-                <Card src="public/emu.jpg"/>
-                <Card src="public/lion.jpg"/>
-                <Card src="public/zebra.jpg"/>
-                <Card src="public/emu.jpg"/>
-                <Card src="public/kenguru.jpg"/>
-                <Card src="public/gepard.jpg"/>
-                <Card src="public/lion.jpg"/>
+                {shuffledData.map((item) => (
+                    <Card src={item.src} alt={item.alt} />
+                ))}
             </div>
 
         </div>
-    )
+    );
 }
